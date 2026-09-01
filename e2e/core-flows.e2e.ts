00c62d55fd90063fa43b2fae3013b9dbb624e284
Gunterlie <hey@gunterlie.de>
2026-09-01 17:18:05 +0200

    test(e2e): update UI store version assertion

--- e2e/core-flows.e2e.ts
+++ e2e/core-flows.e2e.ts
@@ -6961,7 +6961,7 @@ test("legacy browser records are cleaned while extension imports stay locked", a
         };
       }),
     )
-    .toEqual({ version: 97, hasExtensionRecords: false, hasCleanupFlag: false });
+    .toEqual({ version: 98, hasExtensionRecords: false, hasCleanupFlag: false });
 
   expect(
     await page.evaluate(
